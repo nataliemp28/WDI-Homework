3779a6d046f4e263257fae9f537a10e547bd6b77
Natalie Pateman <nataliempateman@gmail.com>
2016-10-18 21:50:11 +0100

    final quotes homework

--- nataliemp28/w04d02/quotes_machine/data/quotes.js
+++ nataliemp28/w04d02/quotes_machine/data/quotes.js
@@ -1,5 +1,12 @@
 let quotes = [
-  { id: 0, quote: "The time is always right to do what is right", author: "Martin Luther King Jr" },
+  { id: 0, details: "The time is always right to do what is right", author: "Martin Luther King Jr" },
+  { id: 1, details: "Do not take life too seriously. You will never get out of it alive.", author: "Elbert Hubbard" },
+  { id: 2, details: "Get your facts first, then you can distort them as you please.", author: "Mark Twain" },
+  { id: 3, details: "I can resist everything except temptation.", author: "Oscar Wilde" },
+  { id: 4, details: "Procrastination is the art of keeping up with yesterday.", author: "Don Marquis" },
+  { id: 5, details: "Never fight an inanimate object.", author: "P. J. O'Rourke" },
+  { id: 6, details: "Everything I like is either illegal, immoral or fattening.", author: "Alexander Woollcott" },
+  { id: 7, details: "Lead us not into temptation. Just tell us where it is; we'll find it.", author: "Sam Levenson" }
 
 ];
 
