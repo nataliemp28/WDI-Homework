c2bda958a4ac2ae012d96ea01ae0a91048eaa295
Natalie Pateman <nataliempateman@gmail.com>
2016-11-30 18:31:20 +0000

    working countries resource

---
+++ nataliemp28/w10d03/rails-countries-homework/countries/db/migrate/20161130155532_create_countries.rb
@@ -0,0 +1,13 @@
+class CreateCountries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :countries do |t|
+      t.string :name
+      t.string :image
+      t.integer :size
+      t.integer :population
+      t.string :language
+
+      t.timestamps
+    end
+  end
+end
