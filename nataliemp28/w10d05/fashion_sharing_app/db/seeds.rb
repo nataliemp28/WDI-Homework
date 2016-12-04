d72d6f9c42405d3be327fd741b3288c1c8a9835f
Natalie Pateman <nataliempateman@gmail.com>
2016-12-04 20:29:16 +0000

    final rails weekend homework

--- nataliemp28/w10d05/fashion_sharing_app/db/seeds.rb
+++ nataliemp28/w10d05/fashion_sharing_app/db/seeds.rb
@@ -12,4 +12,14 @@ Product.create!([{
   retailer: "Kurt Geiger",
   image: "http://media.topshop.com/wcsstore/TopShop/images/catalog/62K39FBLK_large.jpg",
   price_at_purchase: "129.99"
+},{
+  description: "Biker jacket",
+  retailer: "All Saints",
+  image: "http://sheerluxe.com/sites/default/files/2014/11/all-saints-biker-jackets-9.jpg",
+  price_at_purchase: "450"
+}, {
+  description: "Stilettos",
+  retailer: "Jimmy Choo",
+  image: "https://trr-sales-images-production.s3.amazonaws.com/uploads/landing_page/image/173/JimmyChoo_DesignerShoes_TheRealReal_Luxury_Consignment.jpg",
+  price_at_purchase: "699"  
 }])
